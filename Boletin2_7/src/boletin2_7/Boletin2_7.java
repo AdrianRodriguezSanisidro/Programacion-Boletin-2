fe98f509e98f7516dfc504043045bfe62b6a2e41
Adry <Adry@Sanisidro>
2016-10-04 09:11:00 +0200

    Menos texto

---
+++ Boletin2_7/src/boletin2_7/Boletin2_7.java
@@ -0,0 +1,20 @@
+package boletin2_7;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author AdrianRodriguezSanisidro
+ */
+public class Boletin2_7 {
+
+    public static void main(String[] args) {
+        float n;float i;float t;double renda;
+        n=Float.parseFloat(JOptionPane.showInputDialog("Nominal "));
+        i=Float.parseFloat(JOptionPane.showInputDialog("Interese anual "));
+        t=Float.parseFloat(JOptionPane.showInputDialog("Numero de anos"));
+        renda=n*(i*Math.pow((1+i),t))/(Math.pow((1+i),t)-1);
+        JOptionPane.showMessageDialog(null,"A renda que debe pagar e de ="+ renda);
+    }
+    
+}
