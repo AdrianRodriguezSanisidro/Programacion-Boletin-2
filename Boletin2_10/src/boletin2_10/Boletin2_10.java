bff7a933d2e35c0fac90b629d4ad93fbf6e797ea
AdrianRodriguezSanisidro <Adry@Sanisidro>
2016-10-06 11:17:54 +0200

    Cambiar valores

--- Boletin2_10/src/boletin2_10/Boletin2_10.java
+++ Boletin2_10/src/boletin2_10/Boletin2_10.java
@@ -9,9 +9,9 @@ import javax.swing.JOptionPane;
 public class Boletin2_10 {
 
     public static void main(String[] args) {
-        long euros,b100,b20,b5,b1;
-        euros=Long.parseLong(JOptionPane.showInputDialog("Cantidad de euros "));
-        b100=euros/100;b20=(euros%100)/20;b5=(euros%100%20)/5;b1=euros%100%20%5;
+        double euros;int b100,b20,b5,b1;
+        euros=Double.parseDouble(JOptionPane.showInputDialog("Cantidad de euros "));
+        b100=(int) (euros/100);b20=(int) ((euros%100)/20);b5=(int) ((euros%100%20)/5);b1=(int) (euros%100%20%5);
         JOptionPane.showMessageDialog(null,"Numero de billetes de 100 ="+b100+"\nNumero de billetes de 20 ="+b20+"\nNumero de billetes de 5 ="+b5+"\nNumero de monedas de 1 ="+b1);
     }
     
