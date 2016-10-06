3852acb09242a3422baf0f05994cd9dd479aaa28
AdrianRodriguezSanisidro <Adry@Sanisidro>
2016-10-06 10:57:37 +0200

    Solucionar fallo de ecuacion

--- Boletin2_8/src/boletin2_8/Boletin2_8.java
+++ Boletin2_8/src/boletin2_8/Boletin2_8.java
@@ -11,7 +11,7 @@ public class Boletin2_8 {
     public static void main(String[] args) {
      float gradosC,gradosF,gradosK;
      gradosC=Float.parseFloat(JOptionPane.showInputDialog("Grados centigrados"));
-     gradosF=gradosC+32;
+     gradosF=gradosC*1.8f+32;
      gradosK=gradosC+273.15f;
      JOptionPane.showMessageDialog(null,"Grados Fahrenheit ="+ gradosF+"\nGrados Kelvin ="+ gradosK);
      
