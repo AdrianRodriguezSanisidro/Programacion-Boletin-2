025899596f5e3d0d854b51952447475c1bc119ce
AdrianRodriguezSanisidro <Adry@Sanisidro>
2016-10-06 10:51:03 +0200

    Cambio larfgo

--- Boletin2_7/src/boletin2_7/Boletin2_7.java
+++ Boletin2_7/src/boletin2_7/Boletin2_7.java
@@ -9,10 +9,11 @@ import javax.swing.JOptionPane;
 public class Boletin2_7 {
 
     public static void main(String[] args) {
-        float n;float i;float t;double renda;
+        double n;int i;float t;double renda;
         n=Float.parseFloat(JOptionPane.showInputDialog("Nominal "));
-        i=Float.parseFloat(JOptionPane.showInputDialog("Interese anual "));
+        i=(int) Long.parseLong(JOptionPane.showInputDialog("Interese anual "));
         t=Float.parseFloat(JOptionPane.showInputDialog("Numero de anos"));
+        i=i/1200;t=t*12;
         renda=n*(i*Math.pow((1+i),t))/(Math.pow((1+i),t)-1);
         JOptionPane.showMessageDialog(null,"A renda que debe pagar e de ="+ renda+"€");
     }
